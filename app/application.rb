f0ca769d992462e0e3a427b42c399fe83cd724e9
Wendy Calderon <wendycalderon@gmail.com>
2018-09-18 02:48:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     
   if req.path.match(/items/)
     item_name = req.path.split("/items/").last
-      item = @@items.find{|i| i.name == item_name}
+      item = @@items.detect{|i| i.name == item_name}
        if item != nil
         resp.write item.price
       else
