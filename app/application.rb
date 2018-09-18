e86e32bf8890721087f789ad067edf155172be65
Wendy Calderon <wendycalderon@gmail.com>
2018-09-18 02:40:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-  if req.path=="/items"
+  if req.path.match(/items/)
     #if 
       resp.write "You requested the songs"
   else
